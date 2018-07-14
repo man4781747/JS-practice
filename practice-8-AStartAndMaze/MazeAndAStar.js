8ff430a816165ea20df3d620d48a0b1aa4351bea
man4781747 <man4781747@gmail.com>
2018-07-15 02:21:48 +0800

    AStarAndMaze-V1.0

---
+++ practice-8-AStartAndMaze/MazeAndAStar.js
@@ -0,0 +1,27 @@
+var I_MazeSize_X = 10;
+var I_MazeSize_Y = 10;
+var Ay_MazeMake, Ay_AStar;
+
+function setup() {
+  Ay_MazeMake = new MazeMake(I_MazeSize_X,I_MazeSize_Y);
+  Ay_MazeMake.MazeMakeGo();
+  Ay_MazeMake.MazeShow();
+  Ay_AStar = new AStarMake(I_MazeSize_Y*2-1, I_MazeSize_X*2-1);
+  //console.log(Ay_AStar.Ay_Maze);
+  let WhereWall = Ay_MazeMake.WhereWall()
+  for (let i=0;i<WhereWall.length;i++){
+    Ay_AStar.SetWallPoint(floor(WhereWall[i]/(I_MazeSize_X*2-1)),
+                                WhereWall[i]%(I_MazeSize_X*2-1));
+  }
+
+  Ay_AStar.SetStartPoint(floor(random(I_MazeSize_Y))*2, floor(random(I_MazeSize_X))*2);
+  Ay_AStar.SetEndPoint(floor(random(I_MazeSize_Y)*2),floor(random(I_MazeSize_X))*2);
+  Ay_AStar.AStarGo();
+  Ay_AStar.AStarShow();
+  //console.log(Ay_MazeMake.WhereWall());
+
+}
+
+function draw() {
+
+}
