8bab4283256c740a4d0f73ebe37c038811a756f3
man4781747 <man4781747@gmail.com>
2018-07-15 13:41:41 +0800

    AStar-AddMag

--- practice-7-AStart/AStar.js
+++ practice-7-AStart/AStar.js
@@ -1,19 +1,29 @@
 var Ay_AStar;
 
 function setup() {
-  Ay_AStar = new AStarMake(6, 8);
-  Ay_AStar.SetStartPoint(2, 2);
-  Ay_AStar.SetEndPoint(2, 6);
+  Ay_AStar = new AStarMake(10, 10);
+
+  for (let i=4;i<10;i++) {
+    for (let j=0;j<10;j++) {
+      Ay_AStar.SetMag(j,i,0.1);
+    }
+  }
+/*
   Ay_AStar.SetWallPoint(1, 4);
   Ay_AStar.SetWallPoint(2, 4);
   Ay_AStar.SetWallPoint(3, 3);
   Ay_AStar.SetWallPoint(4, 3);
   Ay_AStar.SetWallPoint(0, 3);
+*/
+  Ay_AStar.SetStartPoint(0, 0);
+  Ay_AStar.SetEndPoint(9, 9);
+
+
 
   Ay_AStar.AStarGo();
   console.log(Ay_AStar);
   noLoop();
-  createCanvas(8 * 100, 6 * 100);
+  createCanvas(800, 800);
   rectMode(CENTER);
 }
 
--- practice-7-AStart/AStarMake.js
+++ practice-7-AStart/AStarMake.js
@@ -15,8 +15,14 @@ function AStarMake(YLen, XLen){
   this.I_END = 0;
   this.minLoc;
 
+  this.SetMag = function (y, x, Mag) {
+    this.Ay_Maze[y][x].Magnification = Mag;
+  }
+
   this.SetStartPoint = function (y, x) {
     this.Ay_Maze[y][x].Type = 'START';
+    this.START_X = x;
+    this.START_Y = y;
     this.Ay_Close.push(y*this.XLen+x);
     this.AddClose(y, x);
   }
@@ -45,11 +51,11 @@ function AStarMake(YLen, XLen){
          this.Ay_Open.indexOf((y-1)*this.XLen+x) == -1 &&
          this.Ay_Close.indexOf((y-1)*this.XLen+x) == -1) {
       this.Ay_Open.push((y-1)*this.XLen+x);
-      this.Ay_Maze[y-1][x].g = 10 + this.Ay_Maze[y][x].g;
+      this.Ay_Maze[y-1][x].g = 10*(this.Ay_Maze[y][x].Magnification) + this.Ay_Maze[y][x].g;
       this.Ay_Maze[y-1][x].f = this.Ay_Maze[y-1][x].h+this.Ay_Maze[y-1][x].g;
       this.Ay_Maze[y-1][x].parent = this.Ay_Maze[y][x];
-    } else if (this.Ay_Open.indexOf((y-1)*this.XLen+x) != -1) {
-      if (this.Ay_Maze[y-1][x].f>(this.Ay_Maze[y][x].f+10)) {
+    } else if (y-1>=0 && this.Ay_Open.indexOf((y-1)*this.XLen+x) != -1) {
+      if (this.Ay_Maze[y-1][x].f>(this.Ay_Maze[y][x].f+10*(this.Ay_Maze[y][x].Magnification))) {
         this.Ay_Maze[y-1][x].parent = this.Ay_Maze[y][x];
       }
     }
@@ -59,11 +65,11 @@ function AStarMake(YLen, XLen){
          this.Ay_Open.indexOf(y*this.XLen+x-1) == -1 &&
          this.Ay_Close.indexOf(y*this.XLen+x-1) == -1) {
       this.Ay_Open.push(y*this.XLen+x-1);
-      this.Ay_Maze[y][x-1].g = 10 + this.Ay_Maze[y][x].g;
+      this.Ay_Maze[y][x-1].g = 10*(this.Ay_Maze[y][x].Magnification) + this.Ay_Maze[y][x].g;
       this.Ay_Maze[y][x-1].f = this.Ay_Maze[y][x-1].h+this.Ay_Maze[y][x-1].g;
       this.Ay_Maze[y][x-1].parent = this.Ay_Maze[y][x];
-    } else if (this.Ay_Open.indexOf((y)*this.XLen+x-1) != -1) {
-      if (this.Ay_Maze[y][x-1].f>this.Ay_Maze[y][x].f+10) {
+    } else if (x-1>=0 && this.Ay_Open.indexOf((y)*this.XLen+x-1) != -1) {
+      if (this.Ay_Maze[y][x-1].f>this.Ay_Maze[y][x].f+10*(this.Ay_Maze[y][x].Magnification)) {
         this.Ay_Maze[y][x-1].parent = this.Ay_Maze[y][x];
       }
     }
@@ -73,11 +79,11 @@ function AStarMake(YLen, XLen){
          this.Ay_Open.indexOf((y+1)*this.XLen+x) == -1 &&
          this.Ay_Close.indexOf((y+1)*this.XLen+x) == -1) {
       this.Ay_Open.push((y+1)*this.XLen+x);
-      this.Ay_Maze[y+1][x].g = 10 + this.Ay_Maze[y][x].g;
+      this.Ay_Maze[y+1][x].g = 10*(this.Ay_Maze[y][x].Magnification) + this.Ay_Maze[y][x].g;
       this.Ay_Maze[y+1][x].f = this.Ay_Maze[y+1][x].h+this.Ay_Maze[y+1][x].g;
       this.Ay_Maze[y+1][x].parent = this.Ay_Maze[y][x];
-    } else if (this.Ay_Open.indexOf((y+1)*this.XLen+x) != -1) {
-      if (this.Ay_Maze[y+1][x].f>this.Ay_Maze[y][x].f+10) {
+    } else if (y+1<this.YLen && this.Ay_Open.indexOf((y+1)*this.XLen+x) != -1) {
+      if (this.Ay_Maze[y+1][x].f>this.Ay_Maze[y][x].f+10*(this.Ay_Maze[y][x].Magnification)) {
         this.Ay_Maze[y+1][x].parent = this.Ay_Maze[y][x];
       }
     }
@@ -87,11 +93,11 @@ function AStarMake(YLen, XLen){
          this.Ay_Open.indexOf(y*this.XLen+x+1) == -1 &&
          this.Ay_Close.indexOf(y*this.XLen+x+1) == -1) {
       this.Ay_Open.push(y*this.XLen+x+1);
-      this.Ay_Maze[y][x+1].g = 10 + this.Ay_Maze[y][x].g;
+      this.Ay_Maze[y][x+1].g = 10*(this.Ay_Maze[y][x].Magnification) + this.Ay_Maze[y][x].g;
       this.Ay_Maze[y][x+1].f = this.Ay_Maze[y][x+1].h+this.Ay_Maze[y][x+1].g;
       this.Ay_Maze[y][x+1].parent = this.Ay_Maze[y][x];
-    } else if (this.Ay_Open.indexOf((y)*this.XLen+x+1) != -1) {
-      if (this.Ay_Maze[y][x+1].f>this.Ay_Maze[y][x].f+10) {
+    } else if (x+1<this.XLen && this.Ay_Open.indexOf((y)*this.XLen+x+1) != -1) {
+      if (this.Ay_Maze[y][x+1].f>this.Ay_Maze[y][x].f+10*(this.Ay_Maze[y][x].Magnification)) {
         this.Ay_Maze[y][x+1].parent = this.Ay_Maze[y][x];
       }
     }
@@ -116,13 +122,16 @@ function AStarMake(YLen, XLen){
       this.AddClose(floor(this.minLoc/this.XLen), this.minLoc%this.XLen)
     }
 
-    /*
+/*
     this.AddOpen();
     this.AddClose(floor(this.minLoc/this.XLen), this.minLoc%this.XLen)
-    */
+*/
   }
 
   this.AStarShow = function() {
+    let F_L = 800./this.XLen;
+    let F_H = 800./this.YLen;
+
     background(0);
     fill(255);
     for (let i=0;i<this.YLen;i++){
@@ -136,44 +145,55 @@ function AStarMake(YLen, XLen){
         } else if (this.Ay_Maze[i][j].Type == 'Wall') {
           fill(0,0,255)
         }
-        rect(50+j*100, 50+i*100, 100-1 ,100-1);
+        rect((j+0.5)*F_L, (i+0.5)*F_H, F_L ,F_H);
         fill(0);
-        text(this.Ay_Maze[i][j].h, 5+j*100, 15+i*100);
-        text(this.Ay_Maze[i][j].g, 85+j*100, 15+i*100);
-        text(this.Ay_Maze[i][j].f, 85+j*100, 75+i*100);
-        text(i*this.XLen+j, 5+j*100, 75+i*100);
+        text(this.Ay_Maze[i][j].h, 5+j*F_L, (F_H-5)+i*F_H);
+        text(this.Ay_Maze[i][j].g, (F_L-15)+j*F_L, 15+i*F_H);
+        text(this.Ay_Maze[i][j].f, (F_L-15)+j*F_L, (F_H-5)+i*F_H);
+        text(i*this.XLen+j, 5+j*F_L, 15+i*F_H);
         pop();
 
         push();
         fill(255,255,0);
         for (let i=0;i<this.Ay_Close.length;i++){
-          ellipse(50+(this.Ay_Close[i]%this.XLen)*100,
-                  50+floor(this.Ay_Close[i]/this.XLen)*100, 10 ,10);
+          ellipse(((this.Ay_Close[i]%this.XLen)+0.5)*F_L,
+                  (floor(this.Ay_Close[i]/this.XLen)+0.5)*F_H, 10 ,10);
         }
         pop();
 
         push();
         fill(0,255,255);
+
         for (let i=0;i<this.Ay_Open.length;i++){
-          ellipse(50+(this.Ay_Open[i]%this.XLen)*100,
-                  50+floor(this.Ay_Open[i]/this.XLen)*100, 10 ,10);
+          ellipse(((this.Ay_Open[i]%this.XLen)+0.5)*F_L,
+                  (floor(this.Ay_Open[i]/this.XLen)+0.5)*F_H, 10 ,10);
         }
         pop();
       }
     }
-
-    LineObj = this.Ay_Maze[this.END_y][this.END_x]
+/*
     push();
-    fill(0);
+    fill(0,0,255);
+    rect((this.START_Y+0.5)*F_L, (this.START_X+0.5)*F_H, F_L ,F_H);
+    fill(0,255,0);
+    rect((this.END_y+0.5)*F_L, (this.END_x+0.5)*F_H, F_L ,F_H);
+    pop();
+*/
+    LineObj = this.Ay_Maze[this.END_y][this.END_x]
 
+    push();
+    stroke(255,0,0);
+    strokeWeight(3);
     while (LineObj.parent != null){
-      line(50+LineObj.x*100,
-           50+LineObj.y*100 ,
-           50+LineObj.parent.x*100,
-           50+LineObj.parent.y*100);
+      line((LineObj.x+0.5)*F_L,
+           (LineObj.y+0.5)*F_H ,
+           (LineObj.parent.x+0.5)*F_L,
+           (LineObj.parent.y+0.5)*F_H);
       LineObj = LineObj.parent;
     }
 
     pop();
+
   }
+
 }
